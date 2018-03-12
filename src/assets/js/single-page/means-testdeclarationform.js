35d2d72d08cf7245d14d6003b75ff2058bbf939d
LuoXu <xu.luo@bizcube.com.sg>
2018-03-12 16:05:30 +0800

    git commit -"update save function"

--- src/assets/js/single-page/means-testdeclarationform.js
+++ src/assets/js/single-page/means-testdeclarationform.js
@@ -1,3 +1,4 @@
+
 $(function(){
   //get cookie & loginID
   var appCookie = Cookies.getJSON('appCookie'),
@@ -5,164 +6,30 @@ $(function(){
       $('#submit').click(function(){
         SaveDeclaretion();
       });
-})
+});
+
 //Submit data
 function SaveDeclaretion(){
-  var sectionA_DeclarationofDate='',sectionA_ordinaryMembership='', sectionA_FamilyName='', sectionA_GivenName='', sectionA_DisplayName='', sectionA_nric='',
-   sectionA_Birth='',sectionA_PostalCode='', sectionA_BlockNo='', sectionA_Level='', sectionA_PostalCode='', sectionA_StreetName='',
-   sectionA_Telhome='', sectionA_Teloffice='', sectionA_Handphone='', sectionA_Occupation='', sectionA_Email='',
-   sectionB_FamilyName='', sectionB_GivenName='', sectionB_DisplayName='', sectionB_nric='', sectionB_Birth='',
-   sectionB_PostalCode='', sectionB_BlockNo='', sectionB_Level='', sectionB_BuildingName='', sectionB_StreetName='',
-   sectionB_mobile='', sectionB_home='', sectionB_office='', sectionB_email='', sectionB_Occupation='',
-   sectionB_relationship='',sectionC_FamilyNameMain='', sectionC_GivenNameMain='', sectionC_DisplayNameMain='',
-   sectionC_AgeMain='',sectionC_OccupationMain='', sectionC_NettMain='', sectionC_GrossMain='', sectionC_FamilyName1='',
-   sectionC_GivenName1='', sectionC_DisplayName1='', sectionC_Age1='', sectionC_Occupation1='',sectionC_Relationship1='',
-   sectionC_Nett1='', sectionC_Gross1='', sectionC_FamilyName2='',sectionC_GivenName2='', sectionC_DisplayName2='',
-   sectionC_Age2='', sectionC_Occupation2='',sectionC_Relationship2='',sectionC_Nett2='', sectionC_Gross2='',
-   sectionC_FamilyName3='',sectionC_GivenName3='', sectionC_DisplayName3='', sectionC_Age3='', sectionC_Occupation3='',
-   sectionC_Relationship3='',sectionC_Nett3='', sectionC_Gross3='', sectionC_FamilyName4='',sectionC_GivenName4='',
-   sectionC_DisplayName4='', sectionC_Age4='', sectionC_Occupation4='',sectionC_Relationship4='',sectionC_Nett4='',
-   sectionC_Gross4='',sectionC_FamilyName5='',sectionC_GivenName5='', sectionC_DisplayName5='', sectionC_Age5='',
-   sectionC_Occupation5='',sectionC_Relationship5='',sectionC_Nett5='', sectionC_Gross5='',sectionD_familysupport='',
-   sectionD_Frequency1='',sectionD_Period1='', sectionD_Amount1='',sectionD_Frequency2='',sectionD_Period2='',
-   sectionD_Amount2='',sectionD_Frequency3='',sectionD_Period3='', sectionD_Amount3='',sectionD_Frequency4='',
-   sectionD_Period4='', sectionD_Amount4='',sectionE_Check='';
-  sectionA_DeclarationofDate=$('#sectionA_DeclarationofDate').val();
-  sectionA_ordinaryMembership=$('#DeclaretionFrom input[name="sectionA_ordinaryMembership"]:checked').val()||'';
-  sectionA_FamilyName=$('#sectionA_FamilyName').val();
-  sectionA_GivenName=$('#sectionA_GivenName').val();
-  sectionA_DisplayName = $('#sectionA_DisplayName').val();
-  sectionA_nric = $('#sectionA_nric').val();
-  sectionA_Birth=$('#sectionA_Birth').val();
-  sectionA_PostalCode = $('#sectionA_PostalCode').val();
-  sectionA_BlockNo = $('#sectionA_BlockNo').val();
-  sectionA_Level = $('#sectionA_Level').val();
-  sectionA_PostalCode = $('#sectionA_PostalCode').val();
-  sectionA_StreetName = $('#sectionA_StreetName').val();
-  sectionA_Telhome = $('#sectionA_Telhome').val();
-  sectionA_Teloffice = $('#sectionA_Teloffice').val();
-  sectionA_Handphone = $('#sectionA_Handphone').val();
-  sectionA_Occupation = $('#sectionA_Occupation').val();
-  sectionA_Email = $('#sectionA_Email').val();
-  sectionB_FamilyName=$('#sectionB_FamilyName').val();
-  sectionB_GivenName=$('#sectionB_GivenName').val();
-  sectionB_DisplayName = $('#sectionB_DisplayName').val();
-  sectionB_nric = $('#sectionB_nric').val();
-  sectionB_Birth=$('#sectionB_dateOfBirthYear').val()+'-'+$('#sectionB_dateOfBirthMonth').val()+'-'+$('#sectionB_dateOfBirthDay').val();
-  sectionB_PostalCode=$('#sectionB_PostalCode').val();
-  sectionB_BlockNo = $('#sectionB_BlockNo').val();
-  sectionB_Level = $('#sectionB_Level').val();
-  sectionB_BuildingName=$('#sectionB_BuildingName').val();
-  sectionB_StreetName = $('#sectionB_StreetName').val();
-  sectionB_mobile = $('#sectionB_mobile').val();
-  sectionB_home = $('#sectionB_home').val();
-  sectionB_office=$('#sectionB_office').val();
-  sectionB_email = $('#sectionB_email').val();
-  sectionB_Occupation = $('#sectionB_Occupation').val();
-  sectionB_relationship=$('#sectionB_relationship').val();
-  sectionC_FamilyNameMain=$('#sectionC_FamilyNameMain').val();
-  sectionC_GivenNameMain=$('#sectionC_GivenNameMain').val();
-  sectionC_DisplayNameMain = $('#sectionC_DisplayNameMain').val();
-  sectionC_AgeMain = $('#sectionC_AgeMain').val();
-  sectionC_OccupationMain=$('#sectionC_OccupationMain').val();
-  sectionC_NettMain=$('#sectionC_NettMain').val();
-  sectionC_GrossMain = $('#sectionC_GrossMain').val();
-  sectionC_FamilyName1 = $('#sectionC_FamilyName1').val();
-  sectionC_GivenName1=$('#sectionC_GivenName1').val();
-  sectionC_DisplayName1=$('#sectionC_DisplayName1').val();
-  sectionC_Age1 = $('#sectionC_Age1').val();
-  sectionC_Occupation1 = $('#sectionC_Occupation1').val();
-  sectionC_Relationship1=$('#sectionC_Relationship1').val();
-  sectionC_Nett1 = $('#sectionC_Nett1').val();
-  sectionC_Gross1 = $('#sectionC_Gross1').val();
-  sectionC_FamilyName2 = $('#sectionC_FamilyName2').val();
-  sectionC_GivenName2=$('#sectionC_GivenName2').val();
-  sectionC_DisplayName2=$('#sectionC_DisplayName2').val();
-  sectionC_Age2 = $('#sectionC_Age2').val();
-  sectionC_Occupation2 = $('#sectionC_Occupation2').val();
-  sectionC_Relationship2=$('#sectionC_Relationship2').val();
-  sectionC_Nett2 = $('#sectionC_Nett2').val();
-  sectionC_Gross2 = $('#sectionC_Gross2').val();
-  sectionC_FamilyName3 = $('#sectionC_FamilyName3').val();
-  sectionC_GivenName3=$('#sectionC_GivenName3').val();
-  sectionC_DisplayName3=$('#sectionC_DisplayName3').val();
-  sectionC_Age3 = $('#sectionC_Age3').val();
-  sectionC_Occupation3 = $('#sectionC_Occupation3').val();
-  sectionC_Relationship3=$('#sectionC_Relationship3').val();
-  sectionC_Nett3 = $('#sectionC_Nett3').val();
-  sectionC_Gross3 = $('#sectionC_Gross3').val();
-  sectionC_FamilyName4 = $('#sectionC_FamilyName4').val();
-  sectionC_GivenName4=$('#sectionC_GivenName4').val();
-  sectionC_DisplayName4=$('#sectionC_DisplayName4').val();
-  sectionC_Age4 = $('#sectionC_Age4').val();
-  sectionC_Occupation4 = $('#sectionC_Occupation4').val();
-  sectionC_Relationship4=$('#sectionC_Relationship4').val();
-  sectionC_Nett4 = $('#sectionC_Nett4').val();
-  sectionC_Gross4 = $('#sectionC_Gross4').val();
-  sectionC_FamilyName5 = $('#sectionC_FamilyName5').val();
-  sectionC_GivenName5=$('#sectionC_GivenName5').val();
-  sectionC_DisplayName5=$('#sectionC_DisplayName5').val();
-  sectionC_Age5 = $('#sectionC_Age5').val();
-  sectionC_Occupation5 = $('#sectionC_Occupation5').val();
-  sectionC_Relationship5=$('#sectionC_Relationship5').val();
-  sectionC_Nett5 = $('#sectionC_Nett5').val();
-  sectionC_Gross5 = $('#sectionC_Gross5').val();
-  $('#DeclaretionFrom input[name="sectionD_familysupport"]').each(function () {
-          if ($(this).is(':checked')) {
-                    sectionD_familysupport = $(this).val();
-          }
+  var array=new Array();
+  $('#DeclaretionFrom :input').each(function(){
+    var type=$(this).attr('type'), name= $(this).attr('name'),val=$(this).val();
+    if (type=="radio") {  val=$(':input[type="'+type+'"][name="'+name+'"]:checked').val()||'';};
+    if (type=="checkbox") {
+      var tempVal='';
+      $(':input[type="'+type+'"][name="'+name+'"]').each(function(index,item){
+        if ($(item).prop('checked')==true) {
+          tempVal+=$(item).val()+',';
+        }
+      });
+      val=(tempVal.length>0?tempVal.substr(0,tempVal.length-1):'');
+    };
+    item=name+':'+val;
+    if ($.inArray(item,array)==-1&&name) {array.push(item);}
   });
-  sectionD_Frequency1 = $('#sectionD_Frequency1').val();
-  sectionD_Period1 = $('#sectionD_Period1').val();
-  sectionD_Amount1 = $('#sectionD_Amount1').val();
-  sectionD_Frequency2 = $('#sectionD_Frequency2').val();
-  sectionD_Period2 = $('#sectionD_Period2').val();
-  sectionD_Amount2 = $('#sectionD_Amount2').val();
-  sectionD_Frequency3 = $('#sectionD_Frequency3').val();
-  sectionD_Period3 = $('#sectionD_Period3').val();
-  sectionD_Amount3 = $('#sectionD_Amount3').val();
-  sectionD_Frequency4 = $('#sectionD_Frequency4').val();
-  sectionD_Period4 = $('#sectionD_Period4').val();
-  sectionD_Amount4 = $('#sectionD_Amount4').val();
-  $(":input[name=sectionE_Check]").each(function () {
-            if ($(this).is(':checked')) {
-                sectionE_Check += $(this).val() + ',';
-            }
-  });
-
-  var data = {"sectionA_DeclarationofDate":sectionA_DeclarationofDate, "sectionA_ordinaryMembership": sectionA_ordinaryMembership, "sectionA_FamilyName": sectionA_FamilyName,
-  "sectionA_GivenName": sectionA_GivenName, "sectionA_DisplayName": sectionA_DisplayName, "sectionA_nric": sectionA_nric,
-  "sectionA_Birth": sectionA_Birth, "sectionA_PostalCode": sectionA_PostalCode, "sectionA_BlockNo": sectionA_BlockNo,
-  "sectionA_Level": sectionA_Level, "sectionA_PostalCode": sectionA_PostalCode,"sectionA_StreetName": sectionA_StreetName,
-  "sectionA_Telhome": sectionA_Telhome,"sectionA_Teloffice": sectionA_Teloffice,"sectionA_Handphone": sectionA_Handphone,
-  "sectionA_Occupation": sectionA_Occupation,"sectionA_Email": sectionA_Email,"sectionB_FamilyName": sectionB_FamilyName,
-  "sectionB_GivenName": sectionB_GivenName,"sectionB_DisplayName": sectionB_DisplayName,"sectionB_nric": sectionB_nric,
-  "sectionB_Birth": sectionB_Birth,"sectionB_PostalCode": sectionB_PostalCode,"sectionB_BlockNo": sectionB_BlockNo,
-  "sectionB_Level": sectionB_Level,"sectionB_BuildingName": sectionB_BuildingName,"sectionB_StreetName": sectionB_StreetName,
-  "sectionB_mobile": sectionB_mobile,"sectionB_home": sectionB_home,"sectionB_office": sectionB_office,"sectionB_email":sectionB_email,
-  "sectionB_Occupation": sectionB_Occupation,"sectionB_relationship": sectionB_relationship,"sectionC_FamilyNameMain": sectionC_FamilyNameMain,
-  "sectionC_GivenNameMain": sectionC_GivenNameMain,"sectionC_DisplayNameMain": sectionC_DisplayNameMain,"sectionC_AgeMain": sectionC_AgeMain,
-  "sectionC_OccupationMain": sectionC_OccupationMain,"sectionC_NettMain": sectionC_NettMain,"sectionC_GrossMain": sectionC_GrossMain,
-  "sectionC_FamilyName1": sectionC_FamilyName1, "sectionC_GivenName1": sectionC_GivenName1, "sectionC_DisplayName1": sectionC_DisplayName1,
-  "sectionC_Age1": sectionC_Age1, "sectionC_Occupation1": sectionC_Occupation1, "sectionC_Relationship1": sectionC_Relationship1,
-  "sectionC_Nett1": sectionC_Nett1, "sectionC_Gross1": sectionC_Gross1, "sectionC_FamilyName2": sectionC_FamilyName2,
-  "sectionC_GivenName2": sectionC_GivenName2, "sectionC_DisplayName2": sectionC_DisplayName2, "sectionC_Age2": sectionC_Age2,
-  "sectionC_Occupation2": sectionC_Occupation2, "sectionC_Relationship2": sectionC_Relationship2, "sectionC_Nett2": sectionC_Nett2,
-  "sectionC_Gross2": sectionC_Gross2, "sectionC_FamilyName3": sectionC_FamilyName3, "sectionC_GivenName3": sectionC_GivenName3,
-  "sectionC_DisplayName3": sectionC_DisplayName3, "sectionC_Age3": sectionC_Age3, "sectionC_Occupation3": sectionC_Occupation3,
-  "sectionC_Relationship3": sectionC_Relationship3, "sectionC_Nett3": sectionC_Nett3, "sectionC_Gross3": sectionC_Gross3,
-  "sectionC_FamilyName4": sectionC_FamilyName4, "sectionC_GivenName4": sectionC_GivenName4, "sectionC_DisplayName4": sectionC_DisplayName4,
-  "sectionC_Age4": sectionC_Age4, "sectionC_Occupation4": sectionC_Occupation4, "sectionC_Relationship4": sectionC_Relationship4,
-  "sectionC_Nett4": sectionC_Nett4, "sectionC_Gross4": sectionC_Gross4, "sectionC_FamilyName5": sectionC_FamilyName5,
-  "sectionC_GivenName5": sectionC_GivenName5, "sectionC_DisplayName5": sectionC_DisplayName5, "sectionC_Age5": sectionC_Age5,
-  "sectionC_Relationship5": sectionC_Relationship5, "sectionC_Nett5": sectionC_Nett5, "sectionC_Gross5": sectionC_Gross5,
-  "sectionD_familysupport": sectionD_familysupport, "sectionD_Frequency1": sectionD_Frequency1, "sectionD_Period1": sectionD_Period1,
-  "sectionD_Amount1": sectionD_Amount1, "sectionD_Frequency2": sectionD_Frequency2, "sectionD_Period2": sectionD_Period2,
-  "sectionD_Amount2": sectionD_Amount2, "sectionD_Frequency3": sectionD_Frequency3, "sectionD_Period3": sectionD_Period3,
-  "sectionD_Amount3": sectionD_Amount3, "sectionD_Frequency4": sectionD_Frequency4, "sectionD_Period4": sectionD_Period4,
-  "sectionD_Amount4": sectionD_Amount4, "sectionE_Check": sectionE_Check
-
-};
+  var data={};
+  for (var i = 0; i < array.length; i++) {
+    data[array[i].split(':')[0]]=array[i].split(':')[1];
+  }
   $.ajax({
     url: apiSrc+"BCMain/iCtc1.SaveDeclaretion.json",
     method: "POST",
