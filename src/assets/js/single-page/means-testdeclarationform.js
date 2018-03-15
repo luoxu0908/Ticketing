eca1df17c2bfee8c71d33c5e85981d93bd328c04
LuoXu <xu.luo@bizcube.com.sg>
2018-03-15 09:53:35 +0800

    remove app.js validate

--- src/assets/js/single-page/means-testdeclarationform.js
+++ src/assets/js/single-page/means-testdeclarationform.js
@@ -1,16 +1,11 @@
-var pageresult = 0;
+
 $(function() {
   //get cookie & loginID
   var appCookie = Cookies.getJSON('appCookie'),
     loginID = appCookie.loginID;
+  formOthersInit();formSectionsInit();
   $('#submit').click(function() {
     SaveDeclaretion();
-  });
-  $('#DeclaretionFrom').on('formvalid.zf.abide', function() {
-    pageresult = 1;
-  });
-  $('#DeclaretionFrom').on('forminvalid.zf.abide', function() {
-    pageresult = 0;
   });
   GetDropdownList('.selOccupation', 'Jobs Category');
   GetRelationship('.selRelationship');
@@ -18,8 +13,7 @@ $(function() {
 
 //Submit data
 function SaveDeclaretion() {
-  $('#DeclaretionFrom').foundation('validateForm');
-  if (pageresult == 0) {
+  if (!formSectionValidate(form,1)) {
     return false;
   }
   var data = {};
@@ -178,7 +172,7 @@ function GetRelationship(sel) {
         if (data.d.RetData.Tbl.Rows.length > 0) {
           var lookup = data.d.RetData.Tbl.Rows;
           for (var i = 0; i < lookup.length; i++) {
-            $(id).append('<option value="' + lookup[i].RelKeyAB + '">' + lookup[i].RelKeyAB + '</option>');
+            $(id).append('<option value="' + lookup[i].RelKeyAB + '">' + lookup[i].RelKeyAB  + '</option>');
           }
         }
       } else {
@@ -186,4 +180,105 @@ function GetRelationship(sel) {
       }
     }
   });
+}
+
+
+function formSectionsInit() {
+  $('form.formSection').each(function() {
+    var form = $(this);
+    var fieldsets = form.find('fieldset');
+    var breadcrumbs = form.find('.breadcrumbs');
+    var footer = form.find('footer.buttonsGroup');
+
+    form.data('current-form-index',0);
+
+    breadcrumbs.html('');
+
+    fieldsets.each(function(index) {
+
+      var fieldset = $(this);
+      fieldset.data('fieldset-index',index);
+      breadcrumbs.append('<li><a href="#'+fieldset.prop('id')+'" data-fieldset-index="'+index+'">'+fieldset.find('h2').html()+'</a>').find('li:eq(0) a').addClass('active');
+
+      if(index>0) {
+        fieldset.hide();
+      }
+    });
+
+    breadcrumbs.find('a').click(function() {
+      var thisObj = $(this);
+      var currentIndex = parseInt(form.data('current-form-index'));
+      if (formSectionValidate(form) ) {
+        loadFormSection(thisObj.data('fieldset-index'));
+      }
+      return false;
+    });
+
+    //set buttons
+    footer.find('#previous').hide();
+    footer.find('[class*=submit]').hide();
+
+    footer.find('#previous').click(function() {
+      isAll=0;
+      var currentIndex = parseInt(form.data('current-form-index'));
+      var targetIndex = currentIndex-1;
+
+      if (targetIndex <0) targetIndex=0;
+
+      if (formSectionValidate(form,0) ) {
+        loadFormSection(targetIndex);
+      }
+      return false;
+    });
+    footer.find('#next').click(function() {
+        if (formSectionValidate(form,0)) {
+          var targetIndex = parseInt(form.data('current-form-index')) + 1;
+          if (targetIndex >= fieldsets.length) targetIndex=fieldsets.length-1;
+          loadFormSection(targetIndex);
+        }
+        return false;
+    });
+  });
+}
+
+function formSectionValidate(form,isAll) {
+  var result=0;
+  if (isAll) {
+      $(form).find('fieldset:hidden :input').attr('disabled','disabled');
+  }
+  $(form).foundation('validateForm');
+  $(form).on('formvalid.zf.abide',function(){result=1;});
+  $(form).find('fieldset :input').removeAttr('disabled');
+  return result;
+}
+
+function loadFormSection(index) {
+  //set index
+  form.data('current-form-index', index);
+  var targetIndex = index;
+
+  breadcrumbs.find('a').removeClass('active').filter(function() {
+    return ($(this).data('fieldset-index') == targetIndex);
+  }).addClass('active');
+
+  //set fieldset`
+  fieldsets.hide().filter(function() {
+    return ($(this).data('fieldset-index') == targetIndex);
+  }).show();
+
+  if (index == 0) {
+    footer.find('#previous').hide();
+    footer.find('#next').show();
+    footer.find('[class*=submit]').hide();
+  }
+  else if (index == fieldsets.length-1) {
+    footer.find('#previous').show();
+    footer.find('#next').hide();
+    footer.find('[class*=submit]').show();
+  }
+  else {
+    footer.find('#previous').show();
+    footer.find('#next').show();
+    footer.find('[class*=submit]').hide();
+  }
 }
